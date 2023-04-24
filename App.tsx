62605dd6ff4fc8761d872c0325afdee666b3d83f
Mykhailo Romaniuk <koalah86@gmail.com>
2023-04-25 02:43:52 +0300

    First Commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
+import Home from './page/Home';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
