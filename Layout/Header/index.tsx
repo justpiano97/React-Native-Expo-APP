2a92c5b3ed5800a9c4de1f0063a54da2405d8555
Mykhailo Romaniuk <koalah86@gmail.com>
2023-04-25 10:35:09 +0300

    Add: Layout structure

---
+++ Layout/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header: React.FC = () => {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  );
+};
+
+export default Header;
