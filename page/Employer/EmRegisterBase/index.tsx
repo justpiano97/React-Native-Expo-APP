15906d9d46f786416380f7920f72eb25a811fea9
Mykhailo Romaniuk <koalah86@gmail.com>
2023-04-26 03:06:40 +0300

    Add: Yup validation in employer registration

--- page/Employer/EmRegisterBase/index.tsx
+++ page/Employer/EmRegisterBase/index.tsx
@@ -1,33 +1,47 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import Input from '../../../components/_ui/Input';
 import Button from '../../../components/_ui/Button';
 import { employerRegisterList } from '../../../utils/constants/employer';
+import { EmployerRegistrationSchema } from '../../../utils/schema';
 
 const EmployerRegisterBase: React.FC = () => {
-  const { control } = useForm();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(EmployerRegistrationSchema) });
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
     <View style={{ flex: 1, backgroundColor: '#3f3f3f' }}>
       <Text style={styles.title}>Employer Registration</Text>
-      <View
+      <ScrollView
         style={{
-          display: 'flex',
-          flexDirection: 'column',
           height: '100%',
-          flex: 1,
-          justifyContent: 'space-between',
+          paddingHorizontal: 30,
         }}
       >
         <View style={styles.inputWrapper}>
-          <Input placeholder="Choose Your Sector" control={control} name="sector" />
+          <Input placeholder="Choose Your Sector" control={control} name="sector" error={errors.sector} />
           {employerRegisterList?.map((item) => (
-            <Input key={item?.label} placeholder={item.label} name={item.name} control={control} />
+            <Input
+              key={item?.label}
+              placeholder={item.label}
+              name={item.name}
+              control={control}
+              error={errors[item?.name]}
+            />
           ))}
         </View>
-        <Button>Continue</Button>
-      </View>
+        <Button onPress={handleSubmit(onSubmit)}>Continue</Button>
+      </ScrollView>
     </View>
   );
 };
--- utils/schema/index.ts
+++ utils/schema/index.ts
@@ -11,3 +11,13 @@ export const ShiftRegistrationSchema = yup.object().shape({
   email: yup.string().required('Email is required field').email('Invalid Email'),
   password: yup.string().required('Password is required field'),
 });
+
+export const EmployerRegistrationSchema = yup.object().shape({
+  sector: yup.string().required('Sector is required field'),
+  contact_name: yup.string().required('Contact name is required field'),
+  business_name: yup.string().required('Business name is required field'),
+  postcode: yup.string().required('Postcode is required field'),
+  contact_number: yup.string().required('Contact number is required field'),
+  email: yup.string().required('Email is required field').email('Invalid Email'),
+  password: yup.string().required('Password is required field'),
+});
